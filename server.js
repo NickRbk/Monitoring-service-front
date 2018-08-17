a3f0b63e76e5ae98fcde1c755cc189addddd9395
Nick Rybak <nrybak94@gmail.com>
2018-08-17 23:51:03 +0300

    dev: prepared app for deploying

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+bodyParser = require('body-parser');
+
+const app = express(),
+  port = process.env.PORT || 4200,
+  host = process.env.HOST || 'localhost';
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.use(express.static(__dirname + '/dist/monitoring-service-front'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/dist/monitoring-service-front/index.html'))
+});
+
+app.listen(port, host, () => {
+  console.log(`App running`);
+});
--- src/app/shared/constants/env.const.ts
+++ src/app/shared/constants/env.const.ts
@@ -2,5 +2,6 @@ import {Injectable} from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class EnvConst {
-  BACKEND_URL = 'http://localhost:8080';
+  // BACKEND_URL = 'http://localhost:8080';
+  BACKEND_URL = 'https://blooming-harbor-89191.herokuapp.com';
 }
