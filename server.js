c1b54dfb3a0fa097f88836c92798b236191d2b8c
Nick Rybak <nrybak1994@gmail.com>
2018-08-20 14:19:36 +0300

    dev: fix cors isuue

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const app = express(),
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
+// configure our app to handle CORS requests
+app.use( (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  res.set({ 'content-type': 'application/json; charset=UTF-8' });
+  next();
+});
+
 app.use(express.static(__dirname + '/dist/monitoring-service-front'));
 
 app.get('/*', (req, res) => {
