1b3dbc0ea74437665e0546b65e84126364576f04
Nick Rybak <nrybak1994@gmail.com>
2018-08-20 14:24:59 +0300

    dev: ...

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use( (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
-  res.set({ 'content-type': 'application/json; charset=UTF-8' });
   next();
 });
 
